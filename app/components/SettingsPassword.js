b9db2f5c2517e0fc4f78cf578840265154795ae0
Nilotpal Saha <nilotpal.saha00@gmail.com>
2023-09-02 14:53:40 +0530

    + password change fixed

--- app/components/SettingsPassword.js
+++ app/components/SettingsPassword.js
@@ -1,6 +1,34 @@
-import { CoreForm, FORM_EDIT_MODE, FORM_IDS, CoreSection, CoreClasses } from "@wrappid/core";
+import * as React from "react";
+import { coreUseNavigate, CoreDialogContext, CoreForm, FORM_EDIT_MODE, FORM_IDS, CoreSection, CoreClasses } from "@wrappid/core";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SettingsPassword() {
+  const navigate = coreUseNavigate();
+  const dispatch = useDispatch();
+  const { setDialog } = React.useContext(CoreDialogContext);
+  const { changePasswordSuccess } = useSelector(state => state.auth);
+
+
+  React.useEffect(() => {
+    // clear changePasswordSuccess state
+    if (changePasswordSuccess) {
+      dispatch({
+        type: "RESET_CHANGE_PASSWORD_STATUS"
+      });
+      setDialog({
+        noCancelButton: true,
+        doneButtonLabel: "Re-Login",
+        doneButton: () => {
+          navigate("/logout");
+        },
+        showDialog: true,
+        subtitle  : "Password changed successfully.",
+        title     : "Success",
+        type      : "success"
+      });
+    }
+  }, [changePasswordSuccess]);
+
   return (
     <CoreSection heading="Change Password" styleClasses={[CoreClasses.COLOR.TEXT_SECONDARY_DARK]}>
       <CoreForm
