88713efaeb22d941a08fa484943277c9b9212138
PritamIT2023 <pritammondal8981@email.com>
2024-01-08 11:02:37 +0530

    feat: Add Models
    SettingMeta model added
    
    Ref: #9

--- service/models.registry.js
+++ service/models.registry.js
@@ -1,3 +1,9 @@
-const modelsRegistry = {};
+const SettingMeta = require("./models/SettingMeta.model");
+const modelsRegistry = {
+    "SettingMeta":{
+        database: "application",
+        model: SettingMeta
+    }
+};
 
 exports.modelsRegistry = modelsRegistry;
