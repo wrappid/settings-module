7725f1bfb5c94a795de73028263eaec265773087
anantakumarghosh <antaghosh@gmail.com>
2024-07-17 13:02:07 +0530

    feat(core): :sparkles: appearance themeSelector added
    appearance themeSelector added
    
    Ref: #35

--- app/components/SettingsNotification.js
+++ app/components/SettingsNotification.js
@@ -13,66 +13,16 @@ import {
   CoreTypographyBody1
 } from "@wrappid/core";
 
+import CustomSectionContent from "./CustomSectionContent";
+
 export default function SettingsNotification() {
   return (
     <>
-      {/* <CoreSection heading="Email Notifications">
+      <CustomSectionContent>
         <CoreTypographyBody1>
-          Enter email addresses that will receive email notifications regarding
-          payments, settlements, reports, webhooks, etc.
-        </CoreTypographyBody1>
-
-        <CoreForm
-          apiMode={"create"}
-          onMountRead={false}
-          formId={FORM_IDS.__UPDATE_EMAIL_NOTIFICATIONS}
-          mode={FORM_EDIT_MODE}
-        />
-      </CoreSection>
-
-      <CoreSection
-        heading={
-          <CoreBox>
-            <CoreFormGroup aria-label="position" row>
-              <CoreFormControlLabel
-                value="start"
-                control={<CoreSwitch defaultChecked />}
-                label={<CoreH5>Enable SMS Notifications</CoreH5>}
-                labelPlacement="start"
-              />
-            </CoreFormGroup>
-          </CoreBox>
-        }
-      >
-        <CoreTypographyBody1>
-          Enter mobile numbers that will receive sms notifications regarding
-          payments, settlements, reports etc.
-        </CoreTypographyBody1>
-
-        <CoreForm
-          apiMode={"create"}
-          onMountRead={false}
-          formId={FORM_IDS.__UPDATE_SMS_NOTIFICATIONS}
-          mode={FORM_EDIT_MODE}
-        />
-      </CoreSection>
-
-      <CoreSection heading="WhatsApp Notifications">
-        <CoreTypographyBody1>
-          Enter mobile numbers that will receive whatsapp notifications
-          regarding payments, settlements, reports etc.
-        </CoreTypographyBody1>
-
-        <CoreForm
-          apiMode={"create"}
-          onMountRead={false}
-          formId={FORM_IDS.__UPDATE_WHATSAPP_NOTIFICATIONS}
-          mode={FORM_EDIT_MODE}
-        />
-      </CoreSection> */}
-      <CoreTypographyBody1>
         Comming Soon...
-      </CoreTypographyBody1>
+        </CoreTypographyBody1>
+      </CustomSectionContent>
     </>
   );
 }
